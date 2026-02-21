637655570abd758c0e77e52ed51b4ba850870e20
DevAryanSin <pineapplepenguin30@gmail.com>
2026-02-21 18:36:03 +0530

    Upload documents

--- Integration Module/main.py
+++ Integration Module/main.py
@@ -6,6 +6,7 @@ import uvicorn
 # Import routers
 from routes.gmail_routes import router as gmail_router
 from routes.slack_routes import router as slack_router
+from routes.pdf_routes import router as pdf_router
 
 # Load environment variables
 load_dotenv()
@@ -15,6 +16,7 @@ app = FastAPI(title="Integration Module API")
 # Include Routers
 app.include_router(gmail_router)
 app.include_router(slack_router)
+app.include_router(pdf_router)
 
 @app.get("/")
 def read_root():
@@ -22,7 +24,8 @@ def read_root():
         "message": "Welcome to the Integration Module API.",
         "endpoints": {
             "gmail": "/gmail/login",
-            "slack": "/slack/login"
+            "slack": "/slack/login",
+            "pdf": "/pdf/parse"
         }
     }
 
